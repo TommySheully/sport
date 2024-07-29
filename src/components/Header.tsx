e442d82bdb1b4560902fb7b22225ba661e7d2a39
Tommy <dfgthii@gmail.com>
2024-07-30 00:09:16 +0300

    feat: add image for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
+import React, { useState } from 'react';
+import { arrow_down, competitions, home, live, logo, market, org, rating, ru } from '@/assets';
+
+
 function Header() {
+  const pages = [
+    {title: 'home', icon: home},
+    {title: 'market', icon: market},
+    {title: 'rating', icon: rating},
+    {title: 'competitions', icon: competitions},
+    {title:'org', icon: org},
+    {title: 'live', icon: live}
+  ];
 
-
-  return (
-    <div>
-
-    </div>
-  )
+  return <div></div>
 }
 
 export default Header
